2c0363974d6dacc7ef6b2b4f39e150b57c80eaa9
V0idCanvas <34862994+V0idCanvas@users.noreply.github.com>
2017-12-28 17:16:13 +0100

    Update srub.c
    Fixed intendation

--- srub.c
+++ srub.c
@@ -16,23 +16,23 @@ int main ()
 
 int GetStrLen1 (char s[])
 {
-  int len = 0;
+  	int len = 0;
 	for (; s[len] != '\0'; ++len) {}
 	return len;
 }
 
 int GetStrLen2 (char s[])
 {
-  int len = 0;
+  	int len = 0;
 	while (s[len] != '\0') {++len;}
-  return len;
+  	return len;
 }
 
 int GetStrLen3 (char s[])
 {
-  int len = 0;
-  while (1) {if (s[len] != '\0') {++len;} else {break;};}
-  return len;
+	int len = 0;
+	while (1) {if (s[len] != '\0') {++len;} else {break;};}
+	return len;
 }
 
 
