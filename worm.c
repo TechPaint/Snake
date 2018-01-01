62febcf7053669ef8a5e4e1a6cd1e04ff8a8e176
TechPaint <34862994+TechPaint@users.noreply.github.com>
2018-01-01 23:54:35 +0100

    Refactor of old code + Struggle with mem alloc.

--- worm.c
+++ worm.c
@@ -28,61 +28,70 @@ int main ()
 	int world_Y = 0, world_X = 0;
 	int food_max = 0; // Max no. of food items in the map.
 	int food_no = 0; // Current no. of food items in the map.
+
+	// Create game world/w scoreboard.
+	// World is allocated array of pointers. (Each pointer is pointing to world row.)
 	GetWorldSize (&gameActive, &food_max, &world_Y, &world_X, scrnMax_Y, scrnMax_X);
 	if (gameActive == FALSE) {return 1;} // Resolution error. Quit the game.
-
-	// Create game world.
-	int worldSize = world_Y * world_X;
-	char world[world_Y][world_X];
+	char *world[world_Y];
+	for (int i = 0; i < world_Y; ++i)
+	{
+		world[i] = calloc (world_X, sizeof (char));
+	}
 	CreateWorld (world_Y, world_X, world);
-	// Initialize basic variables.
+	// Score is drawn separately from world.
+	int score = 0, score_Y = world_Y / 2, score_X = world_X + 2; 
+
 	char wrm_headTurn;
 	int wrm_head_X, wrm_head_Y, wrm_len;
 	wrm_head_Y = 1;
 	wrm_head_X = 1;
 	wrm_len = -1; //No. of worm segments (head is not segment).
-	int food_YX[worldSize][worldSize]; // Y, X coords of the food.
-	// Prepare coord array (-1 means empty position).
-	IntArrayColumnFill_2D (0, 0, worldSize, worldSize, food_YX, -1);
-	IntArrayColumnFill_2D (1, 0, worldSize, worldSize, food_YX, -1);
+
+	// Y, X coords of the food (pointers to arrays holding them).
+	int *food_YX[2];
+	food_YX[0] = calloc (food_max, sizeof (int));
+	food_YX[1] = calloc (food_max, sizeof (int));
+	
 	// Arrays holding Y, X coords of segments (indexed by wrmLength).
-	int wrm_allPos_Y[worldSize];
-	int wrm_allPos_X[worldSize];
-	ArrayFill_Int (wrm_allPos_Y, worldSize, -1);
-	ArrayFill_Int (wrm_allPos_X, worldSize, -1);
+	int *wrm_allPos_YX[2];
+	wrm_allPos_YX[0] = calloc (1, sizeof (int));
+	wrm_allPos_YX[1] = calloc (1, sizeof (int));
 
 	// Start the game
 	attron (COLOR_PAIR(4));
-	Draw2dArray (world_Y, world_X, world);
+	CharArrayPtr_Draw (world_Y, world_X, world);
 	PlaceWorm(
 		&wrm_head_Y, &wrm_head_X, &wrm_headTurn, 
-		wrm_allPos_Y, wrm_allPos_X, &wrm_len, world_Y, world_X);
+		wrm_allPos_YX, &wrm_len, world_Y, world_X);
 	SpawnFood(
-		wrm_head_Y, wrm_head_X, wrm_allPos_Y, wrm_allPos_X, 
-		wrm_len, world_Y, world_X, food_max, &food_no, worldSize, food_YX);
+		wrm_head_Y, wrm_head_X, wrm_allPos_YX, wrm_len, 
+		world_Y, world_X, food_max, &food_no, food_YX);
 	DrawAssets(
-		wrm_head_Y, wrm_head_X, wrm_headTurn, wrm_allPos_Y, 
-		wrm_allPos_X, wrm_len, food_no, worldSize, food_YX);
+		wrm_head_Y, wrm_head_X, wrm_headTurn, wrm_allPos_YX, wrm_len, food_no, food_YX);
 
 	while (gameActive)
 	{
 		IsItFood(
-			&wrm_head_Y, &wrm_head_X, wrm_allPos_Y, wrm_allPos_X, 
-			&wrm_len, food_max, &food_no, worldSize, food_YX, world_Y, world_X);
+			&wrm_head_Y, &wrm_head_X, wrm_allPos_YX, &wrm_len, 
+			food_max, &food_no, &score, score_Y, score_X, food_YX, world_Y, world_X);
 		InputRespond (&wrm_headTurn);
-		MoveWorm (&wrm_head_Y, &wrm_head_X, &wrm_headTurn, wrm_allPos_Y, wrm_allPos_X, wrm_len);
+		MoveWorm (&wrm_head_Y, &wrm_head_X, &wrm_headTurn, wrm_allPos_YX, wrm_len);
 		DrawAssets(
-			wrm_head_Y, wrm_head_X, wrm_headTurn, wrm_allPos_Y, 
-			wrm_allPos_X, wrm_len, food_no, worldSize, food_YX);
+			wrm_head_Y, wrm_head_X, wrm_headTurn, wrm_allPos_YX, wrm_len, food_no, food_YX);
 		refresh ();
-		gameActive = DidILose (wrm_head_Y, wrm_head_X, wrm_allPos_Y, wrm_allPos_X, wrm_len, world_Y, world_X);
+		gameActive = DidILose (wrm_head_Y, wrm_head_X, wrm_allPos_YX, wrm_len, world_Y, world_X);
 		usleep (wrm_step_len);
 	}
-	endwin ();	// End curses.
-	printf ("Final length: %d", wrm_len + 1);
-
+	/*
+	// Deallocate memory, end game.
+	free (world);
+	free (food_YX);
+	free (wrm_allPos_YX);
+	*/
 	attroff (COLOR_PAIR(4));
 	endwin ();	// End curses.
+	printf ("Final length: %d \n", wrm_len + 1);
 	return 0;
 }
 
