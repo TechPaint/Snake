eed2b71eb03567c00d02a96d6a9cfce7030712be
TechPaint <34862994+TechPaint@users.noreply.github.com>
2018-01-01 23:56:09 +0100

    Refactoring.

--- worm.h
+++ worm.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
 #include <curses.h>
 
 #define SECOND		1000000		// 1 second in NCurses.
-#define SHOW_INTRO		TRUE
+#define SHOW_INTRO		FALSE
 
 #define DIFICULTY_EASY		1
 #define DIFICULTY_MEDIUM		2
@@ -32,44 +33,46 @@
 #define WORLD_SMALL_X		20
 
 
+int *IntArray_append (int add_me, int arr[], int arr_size);
+
+
 /*WORM BEHAVIOR*/
 void InputRespond (char *wrm_headTurn);
 int DidILose(
-	int wrm_head_Y, int wrm_head_X, int wrm_allPos_Y[], 
-	int wrm_allPos_X[], int wrm_len, int world_Y, int world_X);
+	int wrm_head_Y, int wrm_head_X, int *wrm_allPos_YX[], int wrm_len, int world_Y, int world_X);
 void IsItFood(
-	int *wrm_head_Y, int *wrm_head_X, 
-	int wrm_allPos_Y[], int wrm_allPos_X[], int *wrm_len, int food_max,
-	int *food_no, int worldSize, int food_YX[][worldSize], int world_Y, int world_X);
-void AddSegment(int newLoc_Y, int newLoc_X, int *wrm_len, int wrm_allPos_Y[], int wrm_allPos_X[]);
+	int *wrm_head_Y, int *wrm_head_X, int *wrm_allPos_YX[], int *wrm_len, int food_max, 
+	int *food_no, int *score, int score_Y, int score_X, int *food_YX[], int world_Y, int world_X);
+void AddSegment (int newLoc_Y, int newLoc_X, int *wrm_len, int *wrm_allPos_YX[]);
 void MoveWorm(
-	int *wrm_head_Y, int *wrm_head_X, char *wrm_headTurn, 
-	int wrm_allPos_Y[], int wrm_allPos_X[], int wrm_len);
-void MoveSegments(int pos_Y, int pos_X, int wrm_allPos_Y[], int wrm_allPos_X[], int wrm_len);
+	int *wrm_head_Y, int *wrm_head_X, char *wrm_headTurn, int *wrm_allPos_YX[], int wrm_len);
+void MoveSegments (int pos_Y, int pos_X, int *wrm_allPos_YX[], int wrm_len);
 
 /*WORLD_GEN*/
 void SpawnFood(
 	int wrm_head_Y, int wrm_head_X, 
-	int wrm_allPos_Y[], int wrm_allPos_X[], int wrm_len, int world_Y, 
-	int world_X, int food_max, int *food_no, int worldSize, int food_YX[][worldSize]);
+	int *wrm_allPos_YX[], int wrm_len, int world_Y, 
+	int world_X, int food_max, int *food_no, int *food_YX[]);
 void PlaceWorm(
 	int *wrm_head_Y, int *wrm_head_X, char *wrm_headTurn, 
-	int wrm_allPos_Y[], int wrm_allPos_X[], int *wrm_len, int world_Y, int world_X);
-void CreateWorld (int world_Y, int world_X, char world[][world_X]);
+	int *wrm_allPos_YX[], int *wrm_len, int world_Y, int world_X);
+void CreateWorld (int world_Y, int world_X, char *world[]);
 
 /*GRAPHICS*/
 void DrawAssets(
-	int wrm_head_Y, int wrm_head_X, char wrm_headTurn, int wrm_allPos_Y[], 
-	int wrm_allPos_X[], int wrm_len, int food_no, int worldSize, int food_YX[][worldSize]);
-void Draw2dArray (int arr_Y, int arr_X, char arr[][arr_X]);
+	int wrm_head_Y, int wrm_head_X, char wrm_headTurn, 
+	int *wrm_allPos_YX[], int wrm_len, int food_no, int *food_YX[]);
 void ShowIntro (int scrnMax_Y, int scrnMax_X);
 void GetDificulty (int *dificulty, int *wrm_step_len, int scrnMax_Y, int scrnMax_X);
-void GetWorldSize (int *gameActive, int *food_max, int *world_Y, int *world_X, int scrnMax_Y, int scrnMax_X);
+void GetWorldSize(
+	int *gameActive, int *food_max, 
+	int *world_Y, int *world_X, int scrnMax_Y, int scrnMax_X);
 
-void CharArrayColumnFill_2D(
-	int lineNo, int startCol, int endCol, 
-	int total_cols, char Array_2D[][total_cols], char fillWithMe);
-void IntArrayColumnFill_2D(
+void CharArray2d_Draw (int arr_Y, int arr_X, char arr[][arr_Y]);
+void CharArrayPtr_Draw (int arr_Y, int arr_X, char *arr[arr_Y]);
+void CharArrayPtr_ColumnFill (int lineNo, int startCol, int endCol, char *Array_2D[], char fillWithMe);
+
+void IntArray_Fill (int array[], int array_length, int fill_with_this);
+void IntArray2d_ColumnFill(
 	int lineNo, int startCol, int endCol, 
 	int total_cols, int Array_2D[][total_cols], int fillWithMe);
-void ArrayFill_Int (int array[], int array_length, int fill_with_this);
